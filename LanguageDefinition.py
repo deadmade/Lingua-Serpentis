ff64c51f52598744ede108f98c659ef83ddd7fa7
ernter4 <124594090+ernter4@users.noreply.github.com>
2025-03-22 15:29:10 +0100

    added language Definition

--- LanguageDefinition.py
+++ LanguageDefinition.py
@@ -1,7 +1,6 @@
 ﻿# Example for Puuuuuul
 
 TOKEN_SPECS = [
-    ("NUMBER", r"\d+"),
     ("IDENTIFIER", r"[a-zA-Z_]\w*"),
     ("ASSIGN", r"="),
     ("PLUS", r"\+"),
@@ -12,4 +11,23 @@ TOKEN_SPECS = [
     ("RPAREN", r"\)"),
     ("NEWLINE", r"\n"),
     ("SKIP", r"[ \t]+"),  # Ignoriere Leerzeichen
+    ("NUMBER", r"°(((X|M||V|I|L)+.?)*(X|M||V|I|L))|nullus"), # Römische Zahlen  ich  kann  halt kei regex bei fragen erklär ichs
+    ("STRING", r"\".*?\""),  # Strings as type
+    ("CHAR","lit"),#littera,
+    ("String","voc"), #vocabulum
+    ("IF", r"si"),
+    ("ELSE", r"aut"),
+    ("WHILE", r"indem"),
+    ("FOR", r"dum"),
+    ("FUNCTION", r"munus"),
+    ("RETURN", r"redde"),
+    ("TRUE", r"verus"),
+    ("FALSE", r"falsus"),
+    ("BREAK",r"abrumpe"),
+    ("CONTINUE",r"continua"),
+    ("PRINT", r"scribe"),
+    ("ISEQUAL", r"par_est"),
+    ("ISNOTEQUAL", r"par_non_est"),
+    ("ISGREATER", r"maior_est"),
+    ("ISLESS",r"minor_est")
 ]
